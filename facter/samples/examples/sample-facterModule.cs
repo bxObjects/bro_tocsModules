00e2c991c708828b5a5d765f8c8083d882530f36
Mohsen Banan <form@mohsen.banan.1.byname.net>
2026-01-13 13:16:24 -0800

    Created an examples directory.

--- facter/samples/examples/sample-facterModule.cs
+++ facter/samples/examples/sample-facterModule.cs
@@ -1,30 +1,9 @@
 #!/usr/bin/env python
 
-####+BEGIN: b:prog:file/particulars :authors ("./inserts/authors-mb.org")
-""" #+begin_org
-* *[[elisp:(org-cycle)][| Particulars |]]* :: Authors, version
-** This File: /bisos/git/auth/bxRepos/bisos-pip/loadAsCs/py3/bin/exmpl-loadAs.cs
-** File True Name: /bisos/git/auth/bxRepos/bisos-pip/loadAsCs/py3/bin/exmpl-loadAs.cs
-** Authors: Mohsen BANAN, http://mohsen.banan.1.byname.net/contact
-#+end_org """
-####+END:
-
-from bisos import b
 from bisos.b import cs
-
-from bisos.b import b_io
-
-from bisos.basics import pathPlus
-from pathlib import Path
-
-# from bisos.loadAsCs import loadAsCs_seed
-from bisos.tocsModules  import facterModule_seed
-
 import collections
 
-facterModule_seed.setup(
-    seedType="common",
-)
+from bisos.tocsModules  import facterModule_seed
 
 def examples_csu() -> None:
 
